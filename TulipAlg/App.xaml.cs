96879067cc8913ad19fcda5e9786cdaf6e8e4d9e
baochun <dreammisc@outlook.com>
2025-10-16 16:48:19 +0800

    初始化

---
+++ TulipAlg/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TulipAlg
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
